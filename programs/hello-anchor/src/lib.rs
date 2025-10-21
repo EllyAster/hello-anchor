4a113c7d8777c02c883ae8f8d97a42c738d5a22a
Elly Aster <asterelly@gmail.com>
2025-10-21 12:31:54 -0400

    init repo

---
+++ programs/hello-anchor/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Bpn8fqtx7rf2YypAfYtrZPFcUboqccy8krZ6GJrzRTas");
+
+#[program]
+pub mod hello_anchor {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
